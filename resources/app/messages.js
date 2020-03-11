f825398cff44f47f95b4ca363c29142813c40205
Calbabreaker <pokemymomxd@gmail.com>
2020-03-11 18:25:39 +1030

    removed session id authen for get messages because unecessery

--- resources/app/messages.js
+++ resources/app/messages.js
@@ -4,26 +4,16 @@ module.exports = async (request, response) => {
   const data = request.params.session_id;
 
   try {
-    const result = await database.checkPropertyInDatabase(
-      {
-        sessionId: data
-      },
-      database.usersDatabase
-    );
-    if (result.status === "Found") {
-      database.messagesDatabase
-        .find({})
-        .sort({ timestamp: 1 })
-        .exec((err, data) => {
-          if (err) {
-            return response.status(400).send(err);
-          } else {
-            return response.json(data);
-          }
-        });
-    } else {
-      return response.status(401).send("ERROR: INVALID SESSION ID");
-    }
+    database.messagesDatabase
+      .find({})
+      .sort({ timestamp: 1 })
+      .exec((err, data) => {
+        if (err) {
+          return response.status(400).send(err);
+        } else {
+          return response.json(data);
+        }
+      });
   } catch (err) {
     console.log(err);
     return response.status(400).send(err);
