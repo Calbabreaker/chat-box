cf6b22421cdd1a14bcc70c7fe3664a4b1995eed5
calbabreaker <calbabreaker@gmail.com>
2020-06-20 22:10:31 +0930

    added tooltip and loading for forms

--- public/js/basicFuncs.js
+++ public/js/basicFuncs.js
@@ -1,6 +1,10 @@
+const loadingIcon = document.getElementById("formLoadingIcon");
+loadingIcon.style.visibility = "hidden";
+
 async function fetchAndHandle(url, textBoxesId, redirect = "") {
   const formatedData = {};
   textBoxesId.forEach((id) => (formatedData[id] = document.getElementById(id).value));
+  loadingIcon.style.visibility = "";
 
   const options = {
     method: "POST",
@@ -15,16 +19,26 @@ async function fetchAndHandle(url, textBoxesId, redirect = "") {
 
   if (response.status === 422) {
     // loop through the errors and sets inputs iccordingly
-    [...document.getElementsByClassName("textInput")].forEach((input) => ((input.style.backgroundColor = "#96ff9e"), input.removeAttribute("title")));
+    [...document.getElementsByClassName("textInput")].forEach((input) => {
+      input.style.backgroundColor = "#96ff9e";
+    });
+
     data.errors.forEach((error) => {
       const whichErrorElement = document.getElementById(error.param);
       whichErrorElement.style.backgroundColor = "#fc9090";
-      whichErrorElement.setAttribute("title", error.msg);
+
+      const tooltip = document.createElement("div");
+      tooltip.classList.add("tooltip");
+      tooltip.textContent = error.msg;
+      whichErrorElement.parentElement.appendChild(tooltip);
     });
   } else if (!data.success) {
     console.log(data.errors);
     alert("There was an unknown problem preventing signing in.");
   } else {
     location.href = redirect;
+    return;
   }
+
+  loadingIcon.style.visibility = "hidden";
 }
